1032670b0ccdc34b73219fd0bb97ddf6f8d9fc58
Micah Hauge <micahhauge@gmail.com>
2017-09-29 15:18:21 -0500

    Added basic project list view

--- new-app/app.js
+++ new-app/app.js
@@ -1,4 +1,10 @@
-
+var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla.";
 function IdeaController($scope) {
     $scope.test = "This is my test message.";
+
+    $scope.projects = [
+        { name: "Android App", description: lorem, contributers: "Micah, Brian, Nathan", startdate: "11/11/2017" },
+        { name: "Music Generation", description: lorem, contributers: "Micah, Brian, Nathan", startdate: "11/11/2017" },
+        { name: "Another Project", description: lorem, contributers: "Micah, Brian, Nathan", startdate: "11/11/2017" },
+    ];
 }
